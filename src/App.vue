0d9c7e7554f8fccc8e6ce614cd69e858b7b7c514
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-07-18 16:48:19 +0600

    final

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,12 @@ export default {
 </script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  *:focus {
+    outline: none;
+  }
 }
 </style>
