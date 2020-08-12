90a17048e691c2000ce1066d0322705e7aaa3652
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-08-12 11:57:17 +0600

    slight mod

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/gym/list">Gym</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -23,21 +23,20 @@
             >
           </b-nav-form>
 
-          <b-nav-item-dropdown text="Lang" right>
+          <!-- <b-nav-item-dropdown text="Lang" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
             <b-dropdown-item href="#">ES</b-dropdown-item>
             <b-dropdown-item href="#">RU</b-dropdown-item>
             <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
+          </b-nav-item-dropdown> -->
 
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
+          <!-- <b-nav-item-dropdown right>
             <template v-slot:button-content>
               <em>User</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+          </b-nav-item-dropdown> -->
         </b-navbar-nav>
         <b-navbar-nav>
           <b-nav-item @click="logout" v-if="isLoggedIn">Logout</b-nav-item>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <GymProblemView></GymProblemView>
+    <div class="info">
+      <h2>CgLow Online Judge</h2>
+      <h4>
+        Developed and maintined by <span><a href="#">TariqueNasrullah</a></span>
+      </h4>
+    </div>
+    <div class="construction">
+      <h5>
+        This site is under construction. You may set a problem
+        <a href="/gym/list">Here</a>
+      </h5>
+    </div>
   </div>
 </template>
 
 <script>
-import GymProblemView from '@/components/GymProblemView.vue'
 export default {
   name: 'Home',
-  components: { GymProblemView }
+  components: {}
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  position: relative;
+  .info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 400px;
+    text-align: center;
+  }
+  .construction {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+  }
+}
+</style>
